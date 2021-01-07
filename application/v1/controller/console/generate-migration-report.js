57c1641c0313cb5755a600f58962f43f58583f92
Jordan <jordmax12@gmail.com>
2021-01-06 21:14:42 -0600

    creates my serverless file, this will allow me to run my stuff locally. might be a bit of overkill, but wanted to expose the power of serverless for this excersise, since i believe its a good tool for this use case

---
+++ application/v1/controller/console/generate-migration-report.js
@@ -0,0 +1,6 @@
+exports.trigger = () => {
+    // Let's start with the new db
+    // for each record in accounts
+    // query the record in the old db
+    // lets see if its different, or if it even exists
+}
